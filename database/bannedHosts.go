6c2b70ec7e0a15e75a934de0ecce022fbf33f89d
Andrey Vassilyev <xavescor@gmail.com>
2023-06-03 06:19:13 +0600

    init base sets in redis

---
+++ database/bannedHosts.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
+)
+
+func initBannedHosts(ctx *context.Context, redisClient *redis.Client) {
+	result, err := redisClient.Type(*ctx, bannedHosts).Result()
+	if err != nil {
+		log.Fatalln("[FATAL]InitRedis: redisClient.Type(ctx, bannedHosts).Result() error", err)
+	}
+	if result != "set" {
+		log.Printf("[WARN]InitRedis: bannedHosts(%s) is not set, rewriting it\n", bannedHosts)
+		err := redisClient.Del(*ctx, bannedHosts).Err()
+		if err != nil {
+			log.Fatalf("[FATAL]InitRedis: cannot remove the bannedHosts(%s) key.\n%s\n", bannedHosts, err)
+		}
+		log.Printf("[INFO]InitRedis: the key bannedHosts(%s) removed\n", bannedHosts)
+		addRes, err := redisClient.SAdd(*ctx, bannedHosts, "").Result()
+		if err != nil || addRes != 1 {
+			log.Fatalf("[FATAL]InitRedis: cannot add the bannedHosts(%s) key.\n%s\n", bannedHosts, err)
+		}
+		log.Printf("[INFO]InitRedis: the SET key bannedHosts(%s) created\n", bannedHosts)
+	} else {
+		log.Printf("[INFO]InitRedis: the SET key bannedHosts(%s) exists\n", bannedHosts)
+	}
+}
--- main.go
+++ main.go
@@ -1,41 +1,37 @@
 package main
 
 import (
-	"context"
-	"github.com/redis/go-redis/v9"
-	"knockknocker/requester"
-	"os"
-	"strconv"
+	"knockknocker/common"
+	"knockknocker/database"
 )
 
-var ctx = context.Background()
-
 func main() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	var cursor uint64
-	count := 0
-	for {
-		var keys []string
-		keys, cursor, _ = rdb.SScan(ctx, "all-hosts", cursor, "", 10).Result()
-		if cursor == 0 {
-			break
-		}
-		for _, dirtyUrl := range keys {
-			count++
-			err := requester.TouchWebsite(dirtyUrl)
-			if err != nil {
-				rdb.SAdd(ctx, "banned", dirtyUrl)
-				println(strconv.Itoa(count) + "|" + dirtyUrl + " is banned")
-			} else {
-				println(strconv.Itoa(count) + "|" + dirtyUrl + " is not banned")
-			}
-		}
-	}
+	database.Init(common.GetEnvs().RedisAddr)
+	//rdb := redis.NewClient(&redis.Options{
+	//	Addr:     os.Getenv("REDIS_ADDR"),
+	//	Password: "", // no password set
+	//	DB:       0,  // use default DB
+	//})
+	//
+	//var cursor uint64
+	//count := 0
+	//for {
+	//	var keys []string
+	//	keys, cursor, _ = rdb.SScan(ctx, "all-hosts", cursor, "", 10).Result()
+	//	if cursor == 0 {
+	//		break
+	//	}
+	//	for _, dirtyUrl := range keys {
+	//		count++
+	//		err := requester.TouchWebsite(dirtyUrl)
+	//		if err != nil {
+	//			rdb.SAdd(ctx, "banned", dirtyUrl)
+	//			println(strconv.Itoa(count) + "|" + dirtyUrl + " is banned")
+	//		} else {
+	//			println(strconv.Itoa(count) + "|" + dirtyUrl + " is not banned")
+	//		}
+	//	}
+	//}
 
 	println("done")
 }
